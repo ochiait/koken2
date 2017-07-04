8771c7bb3afc55775b9e730ecf5a38a377a6634e
Tetsuya OCHIAI <ochiai@ryorika-mm.jp>
2017-07-04 15:33:27 +0900

    Railsからメールを送信しよう。 #22

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   resources :wards, :except => [:show]
   devise_for :guardians
   resources :guardians, :except => [:index, :new]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
