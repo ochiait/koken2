3b15196b553e8cb3ddff984460d31b098b3a1338
Tetsuya OCHIAI <ochiai@ryorika-mm.jp>
2017-07-24 10:13:26 +0900

    Scopeを使ってみよう。 #31

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,7 +4,7 @@ class ActivitiesController < ApplicationController
   # GET /activities
   # GET /activities.json
   def index
-    @activities = Activity.all
+    @activities = Activity.oneyear
   end
 
   # GET /activities/new
